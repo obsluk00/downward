89bba124971ee9f7f6fa56fed3ae942407d355f2
obsluk00 <luka.obser@unibas.ch>
2023-06-27 18:03:21 +0200

    small fix

--- src/search/merge_and_shrink/non_orthogonal_merge_and_shrink_algorithm.cc
+++ src/search/merge_and_shrink/non_orthogonal_merge_and_shrink_algorithm.cc
@@ -350,7 +350,7 @@ void NonOrthogonalMergeAndShrinkAlgorithm::main_loop(
     log << "Main loop runtime: " << timer.get_elapsed_time() << endl;
     log << "Maximum intermediate abstraction size: "
         << maximum_intermediate_size << endl;
-    log << "Times cloned: " << 100 - clone_tokens << endl;
+    log << "Times cloned: " << tokens - clone_tokens << endl;
     shrink_strategy = nullptr;
     label_reduction = nullptr;
 }
