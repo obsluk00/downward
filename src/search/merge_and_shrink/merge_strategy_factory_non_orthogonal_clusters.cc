b563146e733e169253b2451dfac4de0fa9ae6054
obsluk00 <luka.obser@unibas.ch>
2023-08-18 19:39:36 +0200

    combining of clusters to reduce amount of necessary clones first 2 implementations

--- src/search/merge_and_shrink/merge_strategy_factory_non_orthogonal_clusters.cc
+++ src/search/merge_and_shrink/merge_strategy_factory_non_orthogonal_clusters.cc
@@ -7,12 +7,12 @@
 
 #include "../task_proxy.h"
 
-#include "../algorithms/sccs.h"
 #include "../plugins/plugin.h"
 #include "../task_utils/causal_graph.h"
 #include "../utils/logging.h"
 #include "../utils/markup.h"
 #include "../utils/system.h"
+#include "../utils/rng_options.h"
 
 #include <algorithm>
 #include <cassert>
@@ -21,11 +21,16 @@
 
 using namespace std;
 
+namespace utils {
+    class RandomNumberGenerator;
+}
+
 namespace merge_and_shrink {
 
     MergeStrategyFactoryNonOrthogonalClusters::MergeStrategyFactoryNonOrthogonalClusters(const plugins::Options &options)
             : MergeStrategyFactory(options),
-              clone_strategy(options.get<CloneStrategy>("clone_strategy")),
+              rng(utils::parse_rng_from_options(options)),
+              combine_strategy(options.get<CombineStrategy>("combine_strategy")),
               cluster_strategy(options.get<ClusterStrategy>("cluster_strategy")),
               tokens(options.get<int>("tokens")),
               merge_selector(options.get<shared_ptr<MergeSelector>>("merge_selector")){
@@ -35,70 +40,65 @@ namespace merge_and_shrink {
             const TaskProxy &task_proxy, const FactoredTransitionSystem &fts) {
         VariablesProxy vars = task_proxy.get_variables();
         causal_graph::CausalGraph cg = task_proxy.get_causal_graph();
-        map<int, int> var_count;
-        for (VariableProxy var : vars) {
-            var_count[var.get_id()] = 0;
-        }
         vector<vector<int>> clusters;
 
         switch(cluster_strategy) {
             case ClusterStrategy::PREDECESSORS:
                 for (VariableProxy var : vars) {
-                    const vector<int> &predecessors = cg.get_predecessors(var.get_id());
-                    for (int var_id : predecessors) {
-                        var_count[var_id] += 1;
-                    }
+                    vector<int> predecessors = cg.get_predecessors(var.get_id());
                     if (predecessors.size() > 0) {
+                        // TODO: this would be a lot nicer if we insert at the right spot (same for other cases)
+                        predecessors.emplace_back(var.get_id());
+                        std::sort(predecessors.begin(), predecessors.end());
                         clusters.push_back(predecessors);
-                        clusters.back().push_back(var.get_id());
                     }
                 }
                 break;
             case ClusterStrategy::SUCCESSORS:
                 for (VariableProxy var : vars) {
-                    const vector<int> &successors = cg.get_successors(var.get_id());
-                    for (int var_id : successors) {
-                        var_count[var_id] += 1;
-                    }
+                    vector<int> successors = cg.get_successors(var.get_id());
                     if (successors.size() > 0) {
+                        successors.emplace_back(var.get_id());
+                        std::sort(successors.begin(), successors.end());
                         clusters.push_back(successors);
-                        clusters.back().push_back(var.get_id());
                     }
                 }
                 break;
             case ClusterStrategy::BOTH:
                 for (VariableProxy var : vars) {
-                    const vector<int> &predecessors = cg.get_predecessors(var.get_id());
-                    const vector<int> &successors = cg.get_successors(var.get_id());
+                    vector<int> predecessors = cg.get_predecessors(var.get_id());
+                    vector<int> successors = cg.get_successors(var.get_id());
                     vector<int> both;
                     set_union(
                             predecessors.begin(), predecessors.end(),
                             successors.begin(), successors.end(),
                             back_inserter(both));
-                    for (int var_id : both) {
-                        var_count[var_id] += 1;
-                    }
                     if (both.size() > 0) {
-                        both.push_back(var.get_id());
+                        both.emplace_back(var.get_id());
+                        std::sort(both.begin(), both.end());
                         clusters.push_back(both);
                     }
                 }
                 break;
         }
 
-        // TODO: clone count case switch. if not enough tokens for all clusters, merge clusters such that the total of required clones decreases
-        switch (clone_strategy) {
-            case CloneStrategy::TOTAL:
-                break;
-            case CloneStrategy::COMBINE_SMALLEST:
-                break;
-            case CloneStrategy::COMBINE_LARGEST:
-                break;
+        map<int, int> var_count = compute_var_count(clusters, task_proxy);
+        int times_to_clone = compute_times_to_clone(var_count, vars.size());
 
+        if (combine_strategy != CombineStrategy::TOTAL) {
+            // TODO: might find a more elegant way for this, needs to be ordered so that equality checks in combination steps work
+            //  -> optimize insertion of variable into its own cluster
+            for (vector<int> cluster : clusters) {
+                sort(cluster.begin(), cluster.end());
+            }
+            while (tokens < times_to_clone) {
+                clusters = combine_clusters(clusters, combine_strategy);
+                var_count = compute_var_count(clusters, task_proxy);
+                times_to_clone = compute_times_to_clone(var_count, vars.size());
+            }
         }
 
-         merge_selector->initialize(task_proxy);
-
+        merge_selector->initialize(task_proxy);
 
         return utils::make_unique_ptr<MergeStrategyNonOrthogonalClusters>(
                 fts,
@@ -107,6 +107,86 @@ namespace merge_and_shrink {
                 move(clusters),
                 move(var_count),
                 tokens);
+    }
+
+    int MergeStrategyFactoryNonOrthogonalClusters::compute_times_to_clone(std::map<int, int> var_count, int variable_count) {
+        int times_to_clone = 0;
+        map<int, int>::iterator it = var_count.begin();
+        while (it != var_count.end()) {
+            times_to_clone += it->second;
+            it++;
+        }
+        return times_to_clone - variable_count;
+    }
+
+    map<int,int> MergeStrategyFactoryNonOrthogonalClusters::compute_var_count(vector<vector<int>> clusters, const TaskProxy &task_proxy) {
+        VariablesProxy vars = task_proxy.get_variables();
+        map<int, int> var_count;
+        for (VariableProxy var : vars) {
+            var_count[var.get_id()] = 0;
+        }
+        for (vector<int> cluster : clusters) {
+            for (int var_id : cluster) {
+                var_count[var_id] += 1;
+            }
+        }
+        return var_count;
+    }
+
+    vector<vector<int>> MergeStrategyFactoryNonOrthogonalClusters::combine_clusters(std::vector<std::vector<int>> clusters,
+                                                                     merge_and_shrink::CombineStrategy combine_strategy) {
+        assert(clusters.size() > 2 && combine_strategy != CombineStrategy::TOTAL);
+        // TODO: shuffle before checking, this also randomly tiebreaks if there are more than 2 smallest/largest
+        int index_1 = 0;
+        int index_2 = 1;
+        switch (combine_strategy) {
+            case CombineStrategy::COMBINE_SMALLEST:
+                for (int i = 0; i < clusters.size(); i++) {
+                    if (clusters[i].size() < clusters[index_1].size()) {
+                        index_2 = index_1;
+                        index_1 = i;
+                    } else if (clusters[i].size() < clusters[index_2].size() && i != index_1)
+                        index_2 = i;
+                }
+                break;
+            case CombineStrategy::COMBINE_LARGEST:
+                for (int i = 0; i < clusters.size(); i++) {
+                    if (clusters[i].size() > clusters[index_1].size()) {
+                        index_2 = index_1;
+                        index_1 = i;
+                    } else if (clusters[i].size() > clusters[index_2].size() && i != index_1)
+                        index_2 = i;
+                }
+                break;
+            case CombineStrategy::RANDOM:
+                // TODO: figure out whats wrong here
+                //first_to_combine = rng->
+                //second_to_combine = rng->choose(clusters);
+                //while (first_to_combine == second_to_combine)
+                //    second_to_combine = rng->choose(clusters);
+                break;
+            case CombineStrategy::LARGEST_OVERLAP:
+                // TODO: implement if its worth it
+                break;
+        }
+        vector<int> combined;
+        set_union(
+                clusters[index_1].begin(), clusters[index_1].end(),
+                clusters[index_2].begin(), clusters[index_2].end(),
+                back_inserter(combined));
+        if (index_1 > index_2) {
+            int temp = index_1;
+            index_1 = index_2;
+            index_2 = temp;
+        }
+        clusters.erase(clusters.begin() + index_1);
+        clusters.erase(clusters.begin() + index_2 - 1);
+        for (vector<int> cluster : clusters) {
+            if (cluster == combined)
+                return clusters;
+        }
+        clusters.emplace_back(combined);
+        return clusters;
     }
 
     bool MergeStrategyFactoryNonOrthogonalClusters::requires_init_distances() const {
@@ -121,15 +201,21 @@ namespace merge_and_shrink {
     void MergeStrategyFactoryNonOrthogonalClusters::dump_strategy_specific_options() const {
         if (log.is_at_least_normal()) {
             log << "Method used to determine how to handle more required clones than available tokens: ";
-            switch (clone_strategy) {
-                case CloneStrategy::TOTAL:
+            switch (combine_strategy) {
+                case CombineStrategy::TOTAL:
                     log << "Ignore the limit";
                     break;
-                case CloneStrategy::COMBINE_LARGEST:
+                case CombineStrategy::COMBINE_LARGEST:
                     log << "Combine the largest clusters";
                     break;
-                case CloneStrategy::COMBINE_SMALLEST:
+                case CombineStrategy::COMBINE_SMALLEST:
                     log << "Combine smallest clusters";
+                    break;
+                case CombineStrategy::RANDOM:
+                    log << "Combine random clusters";
+                    break;
+                case CombineStrategy::LARGEST_OVERLAP:
+                    log << "Combine clusters with largest overlap";
                     break;
             }
             log << endl;
@@ -165,8 +251,8 @@ namespace merge_and_shrink {
             document_title("Non-orthogonal cluster based merge strategy");
             document_synopsis(
                     "lorem ipsum dolor sit amet");
-            add_option<CloneStrategy>(
-                    "clone_strategy",
+            add_option<CombineStrategy>(
+                    "combine_strategy",
                     "how to clone if not enough tokens",
                     "complete");
             add_option<ClusterStrategy>(
@@ -182,6 +268,8 @@ namespace merge_and_shrink {
                     "tokens",
                     "Amount of times the algorithm is allowed to cloned.");
             add_merge_strategy_options_to_feature(*this);
+
+            utils::add_rng_options(*this);
         }
 
         virtual shared_ptr<MergeStrategyFactoryNonOrthogonalClusters> create_component(const plugins::Options &options, const utils::Context &context) const override {
@@ -191,12 +279,16 @@ namespace merge_and_shrink {
 
     static plugins::FeaturePlugin<MergeStrategyFactoryNonOrthogonalClustersFeature> _plugin;
 
-    static plugins::TypedEnumPlugin<CloneStrategy> _enum_plugin1({
-                                                                     {"complete",
+    static plugins::TypedEnumPlugin<CombineStrategy> _enum_plugin1({
+                                                                     {"total",
                                                                              ""},
                                                                      {"combine_smallest",
                                                                              ""},
                                                                      {"combine_largest",
+                                                                             ""},
+                                                                     {"random",
+                                                                             ""},
+                                                                     {"larges_overlap",
                                                                              ""},
                                                              });
     static plugins::TypedEnumPlugin<ClusterStrategy> _enum_plugin({
--- src/search/merge_and_shrink/merge_strategy_factory_non_orthogonal_clusters.h
+++ src/search/merge_and_shrink/merge_strategy_factory_non_orthogonal_clusters.h
@@ -3,15 +3,25 @@
 
 #include "merge_strategy_factory.h"
 
+#include "../utils/rng_options.h"
+
+#include <map>
+
+namespace utils {
+    class RandomNumberGenerator;
+}
+
 namespace merge_and_shrink {
     class MergeTreeFactory;
     class MergeSelector;
 
     // How to distribute tokens
-    enum class CloneStrategy {
+    enum class CombineStrategy {
         TOTAL,
         COMBINE_SMALLEST,
-        COMBINE_LARGEST
+        COMBINE_LARGEST,
+        RANDOM,
+        LARGEST_OVERLAP
     };
 
     // How to create clusters
@@ -23,10 +33,12 @@ namespace merge_and_shrink {
     };
 
     class MergeStrategyFactoryNonOrthogonalClusters : public MergeStrategyFactory {
-        CloneStrategy clone_strategy;
+        const std::shared_ptr<utils::RandomNumberGenerator> rng;
+        CombineStrategy combine_strategy;
         ClusterStrategy cluster_strategy;
         int tokens;
         std::shared_ptr<MergeSelector> merge_selector;
+
     protected:
         virtual std::string name() const override;
         virtual void dump_strategy_specific_options() const override;
@@ -38,6 +50,10 @@ namespace merge_and_shrink {
                 const FactoredTransitionSystem &fts) override;
         virtual bool requires_init_distances() const override;
         virtual bool requires_goal_distances() const override;
+    private:
+        int compute_times_to_clone(std::map<int, int> var_count, int variable_count);
+        std::map<int, int> compute_var_count(std::vector<std::vector<int>> clusters, const TaskProxy &task_proxy);
+        std::vector<std::vector<int>> combine_clusters(std::vector<std::vector<int>> clusters, CombineStrategy combine_strategy);
     };
 }
 
