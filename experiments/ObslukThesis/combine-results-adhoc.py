17aca6c2f34292572b6a13ac0897eadef816ff03
obsluk00 <luka.obser@unibas.ch>
2023-09-11 13:59:24 +0200

    trying to filter big experiment

---
+++ experiments/ObslukThesis/combine-results-adhoc.py
@@ -0,0 +1,116 @@
+#! /usr/bin/env python3
+
+from downward.experiment import FastDownwardExperiment
+from downward.reports import Attribute
+from downward.reports.absolute import AbsoluteReport
+
+non_orthogonality = Attribute("non_orthogonality", absolute=True)
+times_cloned = Attribute("times_cloned", absolute=True)
+average_cloned = Attribute("average_cloned", absolute=True)
+largest_cloned = Attribute("largest_cloned", absolute=True)
+ATTRIBUTES = [
+"cost",
+"coverage",
+"error",
+"evaluations",
+"expansions",
+"expansions_until_last_jump",
+"generated",
+"memory",
+"planner_memory",
+"planner_time",
+"quality",
+"run_dir",
+"score_evaluations",
+"score_expansions",
+"score_generated",
+"score_memory",
+"score_search_time",
+"score_total_time",
+"search_time",
+"total_time",
+non_orthogonality,
+times_cloned,
+average_cloned,
+largest_cloned,
+]
+
+EXTRA_ATTRIBUTES=[
+     Attribute('search_out_of_memory', absolute=True, min_wins=True),
+     Attribute('search_out_of_time', absolute=True, min_wins=True),
+     Attribute('ms_construction_time', absolute=False, min_wins=True,
+function=geometric_mean),
+     Attribute('score_ms_construction_time', min_wins=False, digits=4),
+     Attribute('ms_atomic_construction_time', absolute=False,
+min_wins=True, function=geometric_mean),
+     Attribute('ms_abstraction_constructed', absolute=True, min_wins=False),
+     Attribute('ms_atomic_fts_constructed', absolute=True, min_wins=False),
+     Attribute('ms_out_of_memory', absolute=True, min_wins=True),
+     Attribute('ms_out_of_time', absolute=True, min_wins=True),
+     Attribute('ms_memory_delta', absolute=False, min_wins=True),
+     Attribute('ms_reached_time_limit', absolute=False, min_wins=True),
+
+     Attribute('ms_avg_imperfect_shrinking', absolute=False,
+min_wins=True, function=arithmetic_mean),
+     Attribute('ms_course_imperfect_shrinking', absolute=True),
+     Attribute('ms_course_label_reduction', absolute=True),
+     Attribute('ms_init_h_improvements', absolute=False, min_wins=False),
+     Attribute('ms_not_exact_iteration', absolute=False, min_wins=False),
+     Attribute('ms_one_scc', absolute=True, min_wins=False),
+     Attribute('ms_linear_order', absolute=True, min_wins=True),
+     Attribute('ms_merge_order', absolute=True),
+     Attribute('ms_course_pruning', absolute=True),
+     Attribute('ms_avg_pruning', absolute=False, min_wins=False,
+function=arithmetic_mean),
+     Attribute('ms_tiebreaking_iterations', absolute=True, min_wins=True),
+     Attribute('ms_tiebreaking_total', absolute=True, min_wins=True),
+     Attribute('ms_max_int_abs_size', absolute=False, min_wins=True,
+function=arithmetic_mean),
+]
+
+
+def filter_one(run):
+    return "clone:0 tokens, dfp, no cp, avoid_existing" in run["algorithm"]
+
+def filter_two(run):
+    return "clone:50 tokens, dfp, no cp, avoid_existing" in run["algorithm"]
+
+def filter_three(run):
+    return "clone:0 tokens, dfp, no cp, avoid_existing" in run["algorithm"]
+
+def filter_four(run):
+    return "clone:0 tokens, dfp, no cp, avoid_existing" in run["algorithm"]
+
+exp = FastDownwardExperiment("combined")
+
+exp.add_fetcher("data/2023-08-21-adhoc-cloning-eval", merge=True)
+
+exp.add_report(
+    AbsoluteReport(
+        attributes=ATTRIBUTES + EXTRA_ATTRIBUTES, filter=filter_one
+    ),
+    name="0 tokens",
+)
+
+exp.add_report(
+    AbsoluteReport(
+        attributes=ATTRIBUTES + EXTRA_ATTRIBUTES, filter=filter_two
+    ),
+    name="15 tokens",
+)
+
+exp.add_report(
+    AbsoluteReport(
+        attributes=ATTRIBUTES + EXTRA_ATTRIBUTES, filter=filter_three
+    ),
+    name="50 tokens",
+)
+
+exp.add_report(
+    AbsoluteReport(
+        attributes=ATTRIBUTES + EXTRA_ATTRIBUTES, filter=filter_four
+    ),
+    name="100 tokens",
+)
+
+exp.run_steps()
