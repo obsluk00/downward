12b1fe577a7f999cb124ef4dbc0a4f112c583a72
obsluk00 <luka.obser@unibas.ch>
2023-09-17 15:21:09 +0200

    attributes filtered, display only constellations using avoid_existing

--- experiments/ObslukThesis/combine-results-adhoc.py
+++ experiments/ObslukThesis/combine-results-adhoc.py
@@ -3,25 +3,17 @@
 from downward.experiment import FastDownwardExperiment
 from downward.reports import Attribute
 from downward.reports.absolute import AbsoluteReport
-from lab.reports import geometric_mean, arithmetic_mean
+from lab.reports import geometric_mean, arithmetic_mean, Report
 
 non_orthogonality = Attribute("non_orthogonality", absolute=True)
 times_cloned = Attribute("times_cloned", absolute=True)
 average_cloned = Attribute("average_cloned", absolute=True)
 largest_cloned = Attribute("largest_cloned", absolute=True)
 ATTRIBUTES = [
-"cost",
 "coverage",
 "error",
-"evaluations",
-"expansions",
-"expansions_until_last_jump",
-"generated",
-"memory",
-"planner_memory",
 "planner_time",
 "quality",
-"run_dir",
 "score_evaluations",
 "score_expansions",
 "score_generated",
@@ -29,11 +21,7 @@ ATTRIBUTES = [
 "score_search_time",
 "score_total_time",
 "search_time",
-"total_time",
-non_orthogonality,
-times_cloned,
-average_cloned,
-largest_cloned,
+"total_time"
 ]
 
 EXTRA_ATTRIBUTES=[
@@ -42,88 +30,41 @@ EXTRA_ATTRIBUTES=[
      Attribute('ms_construction_time', absolute=False, min_wins=True,
 function=geometric_mean),
      Attribute('score_ms_construction_time', min_wins=False, digits=4),
-     Attribute('ms_atomic_construction_time', absolute=False,
-min_wins=True, function=geometric_mean),
-     Attribute('ms_abstraction_constructed', absolute=True, min_wins=False),
-     Attribute('ms_atomic_fts_constructed', absolute=True, min_wins=False),
      Attribute('ms_out_of_memory', absolute=True, min_wins=True),
-     Attribute('ms_out_of_time', absolute=True, min_wins=True),
-     Attribute('ms_memory_delta', absolute=False, min_wins=True),
-     Attribute('ms_reached_time_limit', absolute=False, min_wins=True),
-
-     Attribute('ms_avg_imperfect_shrinking', absolute=False,
-min_wins=True, function=arithmetic_mean),
-     Attribute('ms_course_imperfect_shrinking', absolute=True),
-     Attribute('ms_course_label_reduction', absolute=True),
-     Attribute('ms_init_h_improvements', absolute=False, min_wins=False),
-     Attribute('ms_not_exact_iteration', absolute=False, min_wins=False),
-     Attribute('ms_one_scc', absolute=True, min_wins=False),
-     Attribute('ms_linear_order', absolute=True, min_wins=True),
-     Attribute('ms_merge_order', absolute=True),
-     Attribute('ms_course_pruning', absolute=True),
-     Attribute('ms_avg_pruning', absolute=False, min_wins=False,
-function=arithmetic_mean),
-     Attribute('ms_tiebreaking_iterations', absolute=True, min_wins=True),
-     Attribute('ms_tiebreaking_total', absolute=True, min_wins=True),
-     Attribute('ms_max_int_abs_size', absolute=False, min_wins=True,
-function=arithmetic_mean),
+     Attribute('ms_out_of_time', absolute=True, min_wins=True)
 ]
 
 
 def filter_baseline(run):
-    return ("clone:0 tokens, dfp, no cp" in run["algorithm"] 
-        or "clone:0 tokens, dfp, no cp, avoid_existing" in run["algorithm"]
-        or "clone:0 tokens, dfp, offline cp" in run["algorithm"]
+    return ("clone:0 tokens, dfp, no cp, avoid_existing" in run["algorithm"]
         or "clone:0 tokens, dfp, offline cp, avoid_existing" in run["algorithm"]
-        or "clone:0 tokens, dfp, online cp" in run["algorithm"]
         or "clone:0 tokens, dfp, online cp, avoid_existing" in run["algorithm"]
-        or "clone:0 tokens, miasm, no cp" in run["algorithm"] 
         or "clone:0 tokens, miasm, no cp, avoid_existing" in run["algorithm"]
-        or "clone:0 tokens, miasm, offline cp" in run["algorithm"]
         or "clone:0 tokens, miasm, offline cp, avoid_existing" in run["algorithm"]
-        or "clone:0 tokens, miasm, online cp" in run["algorithm"]
         or "clone:0 tokens, miasm, online cp, avoid_existing" in run["algorithm"])
 
 def filter_15_tokens(run):
-    return ("clone:15 tokens, dfp, no cp" in run["algorithm"] 
-        or "clone:15 tokens, dfp, no cp, avoid_existing" in run["algorithm"]
-        or "clone:15 tokens, dfp, offline cp" in run["algorithm"]
+    return ("clone:15 tokens, dfp, no cp, avoid_existing" in run["algorithm"]
         or "clone:15 tokens, dfp, offline cp, avoid_existing" in run["algorithm"]
-        or "clone:15 tokens, dfp, online cp" in run["algorithm"]
         or "clone:15 tokens, dfp, online cp, avoid_existing" in run["algorithm"]
-        or "clone:15 tokens, miasm, no cp" in run["algorithm"] 
         or "clone:15 tokens, miasm, no cp, avoid_existing" in run["algorithm"]
-        or "clone:15 tokens, miasm, offline cp" in run["algorithm"]
         or "clone:15 tokens, miasm, offline cp, avoid_existing" in run["algorithm"]
-        or "clone:15 tokens, miasm, online cp" in run["algorithm"]
         or "clone:15 tokens, miasm, online cp, avoid_existing" in run["algorithm"])
 
 def filter_50_tokens(run):
-    return ("clone:50 tokens, dfp, no cp" in run["algorithm"] 
-        or "clone:50 tokens, dfp, no cp, avoid_existing" in run["algorithm"]
-        or "clone:50 tokens, dfp, offline cp" in run["algorithm"]
-        or "clone:50 tokens, dfp, offline cp, avoid_existing" in run["algorithm"]
-        or "clone:50 tokens, dfp, online cp" in run["algorithm"]
+    return ("clone:50 tokens, dfp, no cp, avoid_existing" in run["algorithm"]
+        or "clone:50 tokens, dfp, offline cp, avoid_existing" in run["algorithm"]-avoid-existing
         or "clone:50 tokens, dfp, online cp, avoid_existing" in run["algorithm"]
-        or "clone:50 tokens, miasm, no cp" in run["algorithm"] 
         or "clone:50 tokens, miasm, no cp, avoid_existing" in run["algorithm"]
-        or "clone:50 tokens, miasm, offline cp" in run["algorithm"]
         or "clone:50 tokens, miasm, offline cp, avoid_existing" in run["algorithm"]
-        or "clone:50 tokens, miasm, online cp" in run["algorithm"]
         or "clone:50 tokens, miasm, online cp, avoid_existing" in run["algorithm"])
 
 def filter_100_tokens(run):
-    return ("clone:100 tokens, dfp, no cp" in run["algorithm"] 
-        or "clone:100 tokens, dfp, no cp, avoid_existing" in run["algorithm"]
-        or "clone:100 tokens, dfp, offline cp" in run["algorithm"]
+    return ("clone:100 tokens, dfp, no cp, avoid_existing" in run["algorithm"]
         or "clone:100 tokens, dfp, offline cp, avoid_existing" in run["algorithm"]
-        or "clone:100 tokens, dfp, online cp" in run["algorithm"]
         or "clone:100 tokens, dfp, online cp, avoid_existing" in run["algorithm"]
-        or "clone:100 tokens, miasm, no cp" in run["algorithm"] 
         or "clone:100 tokens, miasm, no cp, avoid_existing" in run["algorithm"]
-        or "clone:100 tokens, miasm, offline cp" in run["algorithm"]
         or "clone:100 tokens, miasm, offline cp, avoid_existing" in run["algorithm"]
-        or "clone:100 tokens, miasm, online cp" in run["algorithm"]
         or "clone:100 tokens, miasm, online cp, avoid_existing" in run["algorithm"])
 
 exp = FastDownwardExperiment("combined")
@@ -134,28 +75,34 @@ exp.add_report(
     AbsoluteReport(
         attributes=ATTRIBUTES + EXTRA_ATTRIBUTES, filter=filter_baseline
     ),
-    name="baseline-0-tokens",
+    name="baseline-0-tokens-avoid-existing",
 )
 
 exp.add_report(
     AbsoluteReport(
         attributes=ATTRIBUTES + EXTRA_ATTRIBUTES, filter=filter_15_tokens
     ),
-    name="15-tokens",
+    name="15-tokens-avoid-existing",
 )
 
 exp.add_report(
     AbsoluteReport(
         attributes=ATTRIBUTES + EXTRA_ATTRIBUTES, filter=filter_50_tokens
     ),
-    name="50-tokens",
+    name="50-tokens-avoid-existing",
 )
 
 exp.add_report(
     AbsoluteReport(
         attributes=ATTRIBUTES + EXTRA_ATTRIBUTES, filter=filter_100_tokens
     ),
-    name="100-tokens",
+    name="100-tokens-avoid-existing",
+)
+
+exp.add_report(
+    AbsoluteReport(
+        attributes=ATTRIBUTES + EXTRA_ATTRIBUTES, filter=filter_baseline, format="tex"
+    )
 )
 
 exp.run_steps()
